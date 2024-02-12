7f85842085b1ba4bf7f06e8343f91768196c8853
James Xiao <jamesx.main@gmail.com>
2024-02-12 15:37:36 -0500

    Add controls for calculating option metrics multiple times

--- ExactPricingMethod/main.cpp
+++ ExactPricingMethod/main.cpp
@@ -8,10 +8,25 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+bool reRunProgram() {
+	char answer;
+	cout << "Do you want to make another Option Metric Calculation? (Y/N): ";
+	cin >> answer;
+	if (answer == 'Y' || answer == 'y') {
+		system("cls");
+		return true;
+	}
+	return false;
+}
+
 void run() {
-	parametersMatrix paraMatrix;
-	paraMatrix.setParameterRange();
-	paraMatrix.calcOptionMetric();
+	bool run_program = true;
+	while (run_program) {
+		parametersMatrix paraMatrix;
+		paraMatrix.setParameterRange();
+		paraMatrix.calcOptionMetric();
+		run_program = reRunProgram();		
+	}
 }
 
 int main() {
